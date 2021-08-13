ca0c05030ef3a759be7af0de1d52161450a83e50
Alex QA <etercity@gmail.com>
2021-08-13 18:58:12 +0300

    Creating a project structure

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+CHROME_DRIVER = './drivers/chromedriver.exe'
+FIREFOX_DRIVER = './drivers/geckodriver.exe'
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+        browser = webdriver.Chrome(executable_path=CHROME_DRIVER, options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", browser_language)
+        browser = webdriver.Firefox(executable_path=FIREFOX_DRIVER, firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
