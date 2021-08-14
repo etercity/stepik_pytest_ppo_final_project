5d3e92779b82d6dd49b74bd9e060cda80d41a165
Alex QA <etercity@gmail.com>
2021-08-14 15:12:42 +0300

    Test check link exist & output error message

--- test_main_page.py
+++ test_main_page.py
@@ -10,3 +10,8 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(5)
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
