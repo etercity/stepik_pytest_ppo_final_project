50032483013e9a6d43d9fc213a38b04279c6dd1e
Alex QA <etercity@gmail.com>
2021-08-16 22:09:16 +0300

    update some methods

---
+++ pages/product_page.py
@@ -0,0 +1,44 @@
+from .base_page import BasePage
+from .locators import  ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def get_product_name(self):
+        """ Получает имя товара, добавляемого в корзину """
+        product_name = self.browser.find_element(*ProductPageLocators.PRUDUCT_NAME).text
+        return product_name
+
+    def get_price(self):
+        """ Получает цену товара, добавляемого в корзину """
+        price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        return price
+
+    def add_to_basket(self):
+        """ Добавляет товар в корзину """
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        button.click()
+        self.solve_quiz_and_get_code()
+
+
+    def should_be_product_name(self,product_name):
+        """ Проверяет соответствие названия товара в корзине """
+        assert product_name == self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text,\
+        'Product name is not correct!'
+
+    def should_be_price(self, price):
+        """ Проверяет соответствие цены товара в корзине """
+        assert price == self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_PRICE).text,\
+        'Product price is not correct!'
+
+    def should_not_be_success_message(self):
+        """ Проверяет, что элемент отсутствует на странице """
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        """ Проверяет, что элемент исчез со страницы (вообще лучше проверять в два приёма:
+         сначала проверить, что он был, а потом что его не стало)"""
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        'Success message has not disappeared'
+
+
---
+++ test_product_page.py
@@ -0,0 +1,71 @@
+import pytest
+import time
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
+
+url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+promo_codes = ['?promo=offer'+str(code) for code in range(0,10)]
+promo_links = [url+promo for promo in promo_codes]
+promo_links[7] = pytest.param(promo_links[7], marks=pytest.mark.skip)
+
+# @pytest.mark.parametrize('promo_link', promo_links)
+# @pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, url)
+    page.open()
+    product_name = page.get_product_name()
+    price = page.get_price()
+    page.add_to_basket()
+    # time.sleep(7)
+
+    page.should_be_product_name(product_name=product_name)  # проверка соответствия имени товара в корзине
+    page.should_be_price(price=price)   # проверка соответствия цены товара в корзине
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, url)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()    # проверяем, что нет сообщения об успехе
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_not_be_success_message()    # проверяем, что нет сообщения об успехе
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, url)
+    page.open()
+    page.add_to_basket()
+    page.should_be_disappeared()
+
+@pytest.mark.skip
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.skip
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+@pytest.mark.skip
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_in_header()       # переходим в корзину из шапки
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_is_empty() # Ожидаем, что в корзине нет товаров
+    basket_page.should_be_empty_basket_message()    # Ожидаем, что есть текст о том что корзина пуста
+
