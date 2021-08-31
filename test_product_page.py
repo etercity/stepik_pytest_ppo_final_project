8f6549046c928045bea843f41b280faa965baf8e
Alex QA <etercity@gmail.com>
2021-08-31 10:59:06 +0300

    deleted unnecessary

--- test_product_page.py
+++ test_product_page.py
@@ -50,14 +50,6 @@ def test_guest_cant_see_success_message(browser):
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-    # page = ProductPage(browser, url)
-    # page.open()
-    # product_name = page.get_product_name()
-    # price = page.get_price()
-    # page.add_to_basket()
-    #
-    # page.should_be_product_name(product_name=product_name)  # проверка соответствия имени товара в корзине
-    # page.should_be_price(price=price)   # проверка соответствия цены товара в корзине
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -76,51 +68,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_basket_is_empty() # в корзине не должно быть товаров
     basket_page.should_be_empty_basket_message()    # сообщение о том что корзина пуста
 
-# @pytest.mark.user
-# class TestUserAddToBasketFromProductPage():
-#
-#     @pytest.fixture(scope='function', autouse=True)
-#     def setup(self, browser):
-#         # link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-#         # page = LoginPage(browser, link)
-#         # page.open()
-#         # page.register_new_user(str(time.time()) + "@mail.com", 'qwern985ombg')
-#         # page.should_be_authorized_user()
-#         link = "http://selenium1py.pythonanywhere.com/"
-#         self.reg_page = LoginPage(browser, link)
-#         self.reg_page.open()
-#         self.reg_page.go_to_login_page()
-#         self.reg_page.register_new_user(str(time.time()) + "@fakemail.org", "1234567!P")
-#         self.reg_page.should_be_authorized_user()
-#
-#     def test_user_cant_see_success_message(self, browser):
-#         # page = ProductPage(browser, url)
-#         # page.open()
-#         # page.should_not_be_success_message()  # сешн ерор
-#         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#         page = ProductPage(browser, link)
-#         page.open()
-#         page.should_not_be_success_message()
-#
-#     @pytest.mark.need_review
-#     def test_user_can_add_product_to_basket(self, browser):
-#         # page = ProductPage(browser, url)
-#         # page.open()
-#         # product_name = page.get_product_name()
-#         # price = page.get_price()
-#         # page.add_to_basket()
-#         #
-#         # page.should_be_product_name(product_name=product_name)  # валидация имени товара в корзине
-#         # page.should_be_price(price=price)   # валидация цены товара в корзине
-#
-#
-#         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=NewYear2019"
-#         page = ProductPage(browser, link)
-#         page.open()
-#         page.should_be_add_to_basket()
-#         page.solve_quiz_and_get_code()
-#         page.should_be_add_correct_product()
-#         page.should_be_correct_sum()
 
 class TestUserAddToBasketFromProductPage:
 
